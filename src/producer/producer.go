33127d7d40bddbd616588fee0900222dac46d941
jason750718 <zecmer@gmail.com>
2022-01-16 23:43:52 +0800

    Initial commit

---
+++ src/producer/producer.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	// q, err := ch.QueueDeclare(
+	// 	"",    // name
+	// 	false, // durable
+	// 	false, // delete when unused
+	// 	true,  // exclusive
+	// 	false, // no-wait
+	// 	nil,   // arguments
+	// )
+	// failOnError(err, "Failed to declare a queue")
+
+	err = ch.ExchangeDeclare(
+		"hello",  // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	// err = ch.QueueBind(
+	// 	q.Name,  // queue name
+	// 	"",      // routing key
+	// 	"hello", // exchange
+	// 	false,
+	// 	nil,
+	// )
+	// failOnError(err, "Failed to bind a queue")
+
+	body := bodyFrom(os.Args)
+	err = ch.Publish(
+		"hello", // exchange
+		"",      // routing key
+		false,   // mandatory
+		false,   // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	log.Printf("Hello World!!!")
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
+}
